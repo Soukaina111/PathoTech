3e593282b7370b33b5298509c3a73fd1d84aeb4b
Soukaina111 <basmahajar@gmail.com>
2024-09-11 22:39:58 +0100

    view to register a new user

--- resources/views/User/create.blade.php
+++ resources/views/User/create.blade.php
@@ -1,7 +1,6 @@
 @extends('adminlte::page')
 @section('title', 'Ajouter un compte')
 @section('content')
-
     <h2>Register</h2>
     <form method="POST" action="/register">
         {{ csrf_field() }}
@@ -9,7 +8,6 @@
             <label for="name"> Full Name:</label>
             <input type="text" class="form-control" id="name" name="name">
         </div>
-
         <div class="form-group">
             <label for="email">Email:</label>
             <input type="email" class="form-control" id="email" name="email">
@@ -23,7 +21,6 @@
         <div class="form-group">
             <button style="cursor:pointer" type="submit" class="btn btn-primary">Submit</button>
         </div>
-            {{-- @include('partials.formerrors') --}}
     </form>
 
 @endsection
